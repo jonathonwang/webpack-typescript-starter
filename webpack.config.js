8bc2f9edab4623b9d41157904ae7f53500fc79b6
Jonathon Kosla <jonathonkosla@gmail.com>
2016-07-12 14:59:23 -0700

    update gitignore and fix webpack indentation

--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,32 @@ const autoprefixer = require('autoprefixer');
 const WebpackNotifierPlugin = require('webpack-notifier');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {  
-  entry: './src/ts/app.ts',
-  output: {
+	entry: './src/ts/app.ts',
+	output: {
 		path: 'dist/',
-    filename: 'js/app.js'
-  },
+		filename: 'js/app.js'
+	},
   // Turn on sourcemaps
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
-  },
+	devtool: 'source-map',
+	resolve: {
+		extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
+	},
   // Add minification
-  plugins: [
+	plugins: [
 		new WebpackNotifierPlugin({title: 'Webpack', contentImage: 'http://reapp.io/images/webpack.svg'}),
-    new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.UglifyJsPlugin(),
 		new ExtractTextPlugin("css/app.css", {
 			allChunks: true
 		})
-  ],
-  module: {
-    loaders: [
-      { test: /\.ts$/, loader: 'ts' },
+	],
+	module: {
+		loaders: [
+			{ test: /\.ts$/, loader: 'ts' },
 			{ test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/, loader: 'file' },
 			{ test: /\.scss$/, loader: ExtractTextPlugin.extract('style-loader', ['css-loader', 'postcss-loader', 'sass-loader']) }
-    ]
-  },
+		]
+	},
 	postcss() {
-    return [autoprefixer, precss];
-  },
-
+		return [autoprefixer, precss];
+ 	}
 }
